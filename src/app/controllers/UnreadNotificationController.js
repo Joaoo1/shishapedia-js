2b130079170c29a9822ce73e568d6a8f4a6aee4d
Joao <silva.joao1@outlook.com.br>
2021-04-04 02:11:39 -0300

    get only unread notifications from last 7 days

--- src/app/controllers/UnreadNotificationController.js
+++ src/app/controllers/UnreadNotificationController.js
@@ -1,9 +1,20 @@
+import { Op } from 'sequelize';
 import Notification from '../models/Notification';
 
 const NotificationController = {
   async index(req, res) {
+    const date = new Date();
+    date.setDate(date.getDate() - 7);
+
     const notifications = await Notification.findAll({
-      where: { user_id: req.userId, read: false },
+      where: {
+        user_id: req.userId,
+        read: false,
+        created_at: {
+          // Get only notifications of the last 7 days
+          [Op.gt]: date,
+        },
+      },
       order: [['created_at', 'DESC']],
       attributes: ['id', 'message', 'read', 'title', 'created_at'],
     });
